722addde2e1d17c49729dd1f8fcaa25038121a8a
Ovod2013 <Ovod2013@yandex.ru>
2023-05-23 16:36:55 +0300

    задача 34 в работе

--- task34/Program.cs
+++ task34/Program.cs
@@ -4,4 +4,31 @@
 // чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+//объявление метода с вводом длины size массива и границ значений
+int[] Massiv(int size, int minValie, int maxValie) 
+{
+    //определение массива целых чисел
+    int[] num;
+    //определение переменной num и выделение памяти 
+    //для size элементов массива
+    num = new int[size];
+    //заполнение массива в цикле
+    for (int i=0; i<size; i++)
+    {
+        num[i] = new Random().Next(minValie, maxValie+1);
+        Console.WriteLine($"{num[i]}");
+    }
+    return num;
+}
 
+int[] array = Massiv(4,0,1000);
+
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+int ost = number%10;
+if (ost == 0 || ost == 2 || ost == 4 || ost == 6 || ost == 8)
+{
+    Console.WriteLine("Число чётное");
+}
+else Console.WriteLine("Число нечётное");
