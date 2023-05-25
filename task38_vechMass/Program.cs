bcd844aa8780cadd6ac7329923f59fcd80f70679
Ovod2013 <Ovod2013@yandex.ru>
2023-05-25 16:50:53 +0300

    задачи 34, 36 и 38 с числами int работают, с веществ числами разбираюсь

---
+++ task38_vechMass/Program.cs
@@ -0,0 +1,20 @@
+﻿//   //определение массива целых чисел
+     Double num;
+//     //определение переменной num и выделение памяти 
+//     num = new float[2];
+//     //заполнение массива в цикле случайными числами
+//     //в диапазоне от minValie до maxValie
+// Console.WriteLine("Массив вещественных чисел, заданных случайном образом:");
+
+//     for (int i=0; i<2; i++)
+//     {
+//         num = 1;
+//     //    num[i] = new Random().Next(1, 123+1);
+//         Console.Write($"{num[i]}");
+//         Console.Write(" ");
+//     }
+//     return num;// Возврат массива
+
+
+num =1.2;
+Console.WriteLine(num);
